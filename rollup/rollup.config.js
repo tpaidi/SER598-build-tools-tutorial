8fc5226ad2b7a92d8080849d1f6831b457b0a9e0
rnolas96 <rnolas96@asu.edu>
2024-11-24 10:31:55 -0700

    Rollup commit

---
+++ rollup/rollup.config.js
@@ -0,0 +1,26 @@
+import html from '@rollup/plugin-html';
+import css from 'rollup-plugin-css-only';
+import terser from '@rollup/plugin-terser';
+
+import path from 'path';
+import fs from 'fs';
+
+export default {
+  input: './src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'esm',
+    sourcemap: true,
+  },
+  plugins: [
+    html({
+      fileName: 'index.html',
+      template: () => {
+        const htmlPath = path.resolve(__dirname, 'src/index.html');
+        return fs.readFileSync(htmlPath, 'utf8');
+      },
+    }),
+    css({ output: 'bundle.css' }),
+    terser(),
+  ],
+};
